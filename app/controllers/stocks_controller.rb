588b13eac742e725cf0ab0c9d5c51bacbf864f5f
Gene <gorange310@gmail.com>
2022-05-21 00:29:59 +0800

    Add portfolio/stock #new #create

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,19 @@
+class StocksController < ApplicationController
+  def new
+    @portfolio = Portfolio.find(params[:portfolio_id])
+  end
+
+  def create
+    keyword = params[:keyword]
+    @portfolio = Portfolio.find(params[:portfolio_id])
+    result = PortfolioFollowStockService.new(@portfolio, keyword).call
+
+    if result
+      redirect_to portfolios_url(@portfolio)
+    else
+      flash[:alert] = "找不到股號或股名 #{keyword} 或重複追蹤"
+      render :new
+    end
+  end
+
+end
